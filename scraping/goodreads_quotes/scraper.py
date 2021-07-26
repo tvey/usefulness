e1210368cf6e65bf19920a9db43440a95907a971
tvey <tanyavoid@ya.ru>
2021-07-26 22:36:43 +0300

    Quote scraper

---
+++ scraping/goodreads_quotes/scraper.py
@@ -0,0 +1,59 @@
+"""Scrape “Popular Quotes” on Goodreads using Requests-HTML; sync version."""
+
+import os
+import json
+import time
+import random
+
+from requests_html import HTMLSession
+
+BASE_URL = 'https://www.goodreads.com/quotes'
+NUM_PAGES = 100
+URLS = [f'{BASE_URL}?page={i}' for i in range(1, (NUM_PAGES + 1))]
+WAIT_TIME = random.random() * 2
+
+
+def get_page_quotes(session, url):
+    """Collect quotes from a given page and return them as a list of dicts."""
+    r = session.get(url)
+    print(url)
+    quote_elems = r.html.find('.quote')
+    page_quotes = []
+
+    for quote_elem in quote_elems:
+        quote_text_raw = quote_elem.find('.quoteText', first=True)
+        quote = quote_text_raw.text.split('”\n― ')[0].strip(' “')
+        author = quote_elem.find('.authorOrTitle', first=True).text.strip(' ,')
+        tags = [a.text for a in quote_elem.find('.smallText.left a')]
+        result = {
+            'quote': quote,
+            'author': author,
+            'tags': tags,
+        }
+        page_quotes.append(result)
+
+    return page_quotes
+
+
+def main(urls, filename='quotes'):
+    """Gather results of every page and write them into a file."""
+    session = HTMLSession()
+    result = []
+    start = time.perf_counter()
+
+    for i, url in enumerate(urls, start=1):
+        page_quotes = get_page_quotes(session, url)
+        result.extend(page_quotes)
+        time.sleep(WAIT_TIME)
+
+    with open(f'{filename}.json', 'w', encoding='utf-8') as f:
+        json.dump(result, f, indent=4, ensure_ascii=False)
+
+    end = time.perf_counter()
+    print(f'Done in {(end - start):.2f}s')
+
+    return result
+
+
+if __name__ == '__main__':
+    main(URLS)
---
+++ scraping/goodreads_quotes/scraper_async.py
@@ -0,0 +1,75 @@
+"""Scrape “Popular Quotes” on Goodreads using Requests-HTML; async version."""
+
+import os
+import time
+import json
+import random
+import asyncio
+
+from requests_html import AsyncHTMLSession
+
+BASE_URL = 'https://www.goodreads.com/quotes'
+NUM_PAGES = 100
+URLS = [f'{BASE_URL}?page={i}' for i in range(1, (NUM_PAGES + 1))]
+WAIT_TIME = random.random() * 2
+
+
+async def get_response(asession, url):
+    """Get url with async session and return the HTMLResponse."""
+    r = await asession.get(url)
+    print(url)
+    await asyncio.sleep(WAIT_TIME)
+    return r
+
+
+async def await_responses(urls):
+    """Gather awaitables for event loop.
+    A list of results will be returned *in order* thanks to asyncio.gather()
+    """
+    asession = AsyncHTMLSession()
+    awaitables = (get_response(asession, url) for url in urls)
+    return await asyncio.gather(*awaitables)
+
+
+def get_page_quotes(r):
+    """Collect quotes from a given page and return them as a list of dicts."""
+    quote_elems = r.html.find('.quote')
+    page_quotes = []
+
+    for quote_elem in quote_elems:
+        quote_text_raw = quote_elem.find('.quoteText', first=True)
+        quote_text = quote_text_raw.text.split('”\n― ')[0].strip(' “')
+        author = quote_elem.find('.authorOrTitle', first=True).text.strip(' ,')
+        tags = [a.text for a in quote_elem.find('.smallText.left a')]
+        quote = {
+            'quote': quote_text,
+            'author': author,
+            'tags': tags,
+        }
+        page_quotes.append(quote)
+
+    return page_quotes
+
+
+def main(responses, filename='quotes'):
+    """Gather results of every page and write them into a file."""
+    result = []
+
+    for r in responses:
+        page_quotes = get_page_quotes(r)
+        result.extend(page_quotes)
+
+    with open(f'{filename}.json', 'w', encoding='utf-8') as f:
+        json.dump(result, f, indent=4, ensure_ascii=False)
+
+    return result
+
+
+if __name__ == '__main__':
+    start = time.perf_counter()
+    responses = asyncio.run(await_responses(URLS))
+    mid = time.perf_counter()
+    print(f'Collected responses in {(mid - start):.2f}s')
+    main(responses, filename='quotes_async')
+    end = time.perf_counter()
+    print(f'Done in {(end - start):.2f}s')
