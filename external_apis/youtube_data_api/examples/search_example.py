5d4fe747d7c7818330b99f9e23576f830c091a2b
tvey <tanyavoid@ya.ru>
2021-09-15 22:16:19 +0300

    Edit search example

--- external_apis/youtube_data_api/examples/search_example.py
+++ external_apis/youtube_data_api/examples/search_example.py
@@ -2,32 +2,30 @@ import os
 import html
 import json
 
+import dotenv
 import requests
-from dotenv import load_dotenv
 
-load_dotenv()
-
-base_url = 'https://www.googleapis.com/youtube/v3/'
-query = 'piano cat'
-
-params = {
-    'q': query,
-    'key': os.environ.get('YOUTUBE_DATA_API_KEY'),
-    'part': ['snippet'],
-    'maxResults': 50,
-    'order': 'relevance',
-    'regionCode': 'GB',
-}
+dotenv.load_dotenv()
 
 
-def search(query=query):
-    search_url = f'{base_url}search'
-    print(params)
-    r = requests.get(search_url, params=params)
+def search(query='piano cat'):
+    url = 'https://www.googleapis.com/youtube/v3/search'
+    params = {
+        'q': query,
+        'key': os.environ.get('YOUTUBE_DATA_API_KEY'),
+        'part': ['snippet'],
+        'maxResults': 50,
+        'order': 'relevance',
+        'regionCode': 'GB',
+    }
+
+    r = requests.get(url, params=params)
     result = r.json()
 
     if r.status_code == 200:
-        filename = query.replace(' ', '_')
+        forbidden = '\\/:*?"<>|%.,;= '
+        trans_table = str.maketrans(forbidden, '_' * len(forbidden))
+        filename = query.translate(trans_table).strip(' _').replace('__', '_')
         with open(f'{filename}.json', 'w', encoding='utf-8') as f:
             json.dump(result, f, indent=4, ensure_ascii=False)
 
