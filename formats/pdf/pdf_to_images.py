dfe8baa0783dc1c790f1b60b550771bb1db0017b
tvey <liteitt@gmail.com>
2022-11-03 21:37:23 +0300

    Improve pdf to images

--- formats/pdf/pdf_to_images.py
+++ formats/pdf/pdf_to_images.py
@@ -1,15 +1,27 @@
 import os
+import tempfile
 
 import pdf2image
 
 
-def convert_pdf_to_image(file_path):
+def convert_pdf_to_images(file_path):
     directory = os.path.dirname(file_path)
     name = os.path.basename(file_path).rsplit('.')[0]
     new_folder = os.path.join(directory, name)
     os.makedirs(new_folder, exist_ok=True)
 
-    images = pdf2image.convert_from_path(file_path)
+    num_pages = pdf2image.pdfinfo_from_path(file_path).get('Pages')
 
-    for i, image in enumerate(images):
-        image.save(f'{new_folder}/page_{i:03}.jpg', 'JPEG')
+    with tempfile.TemporaryDirectory() as tempdir:
+        for page in range(1, num_pages + 1):
+            image = pdf2image.convert_from_path(
+                file_path,
+                dpi=200,
+                fmt='jpeg',
+                first_page=page,
+                last_page=page,
+                output_folder=tempdir,
+            )[0]
+
+            image.save(f'{new_folder}/page_{page:03}.jpg', 'JPEG')
+            print(f'Saved page {page:03}/{num_pages}')
