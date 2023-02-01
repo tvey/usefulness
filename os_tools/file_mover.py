fa429a8d6242979dda6cdbbf9f42c6e8c3edee8b
tvey <liteitt@gmail.com>
2023-02-01 22:56:56 +0300

    File mover

---
+++ os_tools/file_mover.py
@@ -0,0 +1,24 @@
+import os
+
+
+def move_file_to_same_name_folder(file_path: str):
+    """Accept a full file path, move the file into a eponymous folder."""
+    if not os.path.isfile(file_path):
+        raise ValueError('File path is incorrect')
+
+    parent_folder, filename = os.path.split(file_path)
+    name, ext = os.path.splitext(filename)
+    new_path = os.path.join(parent_folder, name)
+    new_file_path = os.path.join(new_path, filename)
+    os.makedirs(new_path, exist_ok=True)
+
+    try:
+        os.rename(file_path, new_file_path)
+        print(f'Moved: {new_file_path}')
+    except FileExistsError:
+        print('File exists in the folder with the same name')
+        # duplicate_filename = f'{name} (duplicate){ext}'
+        # duplicate_path = os.path.join(parent_folder, duplicate_filename)
+        # os.rename(file_path, duplicate_path)
+
+        # move_file_to_same_name_folder(duplicate_path)
