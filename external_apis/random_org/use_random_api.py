58f67a12022e10769528ca8110e3f0182ca77edc
tvey <liteitt@gmail.com>
2023-11-23 22:52:36 +0300

    Try other random api methods

--- external_apis/random_org/use_random_api.py
+++ external_apis/random_org/use_random_api.py
@@ -14,29 +14,66 @@ headers = {
 }
 
 
-def get_random_integers(n: int, min_value: int, max_value: int) -> list[int]:
+def get_result(method: str, params: dict) -> list:
     data = {
         'jsonrpc': '2.0',
-        'method': 'generateIntegers',
+        'method': method,
         'id': 123,
-        'params': {
-            'apiKey': API_KEY,
-            'n': n,
-            'min': min_value,
-            'max': max_value,
-            'replacement': False,
-        },
+        'params': {'apiKey': API_KEY} | params,
     }
     json_data = json.dumps(data)
-
     r = requests.post(BASIC_API_URL, data=json_data, headers=headers)
     r_data = r.json()
+    print(r_data)
 
     if not r.ok:
         raise Exception(r_data['error']['message'])
 
     try:
-        ints = r_data['result']['random']['data']
-        return ints
+        result = r_data['result']['random']['data']
+        return result
     except KeyError:
         pass
+
+
+def get_random_integers(n: int, min_value: int, max_value: int) -> list[int]:
+    method = 'generateIntegers'
+    params = {
+        'n': n,
+        'min': min_value,
+        'max': max_value,
+        'replacement': False,
+    }
+    return get_result(method, params)
+
+
+def get_random_integer_sequences(
+    n: int,
+    length: int | list[int],
+    min_values: int | list[int],
+    max_values: int | list[int],
+    replacement: bool = False,
+) -> list[list[int]]:
+    method = 'generateIntegerSequences'
+    params = {
+        'n': n,
+        'length': length,
+        'min': min_values,
+        'max': max_values,
+        'replacement': replacement,
+        'base': 10,
+    }
+    return get_result(method, params)
+
+
+def get_random_strings(
+    n: int, length: int, chars: str, replacement: bool = True
+) -> list[str]:
+    method = 'generateStrings'
+    params = {
+        'n': n,
+        'length': length,
+        'characters': chars,
+        'replacement': replacement,
+    }
+    return get_result(method, params)
